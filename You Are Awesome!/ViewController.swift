eb933f3163b05fd2529d4863f35acad0669ebd5e
Scott Rocca <scott.rocca@hp.com>
2022-07-01 16:11:58 -0700

    adding array of messages

--- You Are Awesome!/ViewController.swift
+++ You Are Awesome!/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet weak var messageLabel: UILabel!
     @IBOutlet weak var imageView: UIImageView!
+    var currentMessageIndex = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,20 +19,35 @@ class ViewController: UIViewController {
     
     @IBAction func messageButtonPressed(_ sender: UIButton) {
         print("Button was pressed")
-        let awesomeMessage = "You Are Awesome!"
-        let greatMessage = "You Are Great!"
+        let messages = ["You Are Awesome!",
+                        "You Are Great!",
+                        "You Are Da Bomb!",
+                        "You are Fantastic",
+                        "Fabulous? That's You!"]
+        messageLabel.text = messages[currentMessageIndex]
+        imageView.image = UIImage(named: "image\(currentMessageIndex)")
         
-        if messageLabel.text == awesomeMessage {
-            messageLabel.text = greatMessage
-            imageView.image = UIImage(named: "image2")
-        } else if messageLabel.text == greatMessage {
-            messageLabel.text = "You Are Da Bomb!"
-            imageView.image = UIImage(named: "image1")
-            
-        } else {
-            messageLabel.text = awesomeMessage
-            imageView.image = UIImage(named: "image0")
+        currentMessageIndex += 1
+        if (currentMessageIndex == messages.count ) {
+            currentMessageIndex = 0
         }
+        
+        
+        
+//        let awesomeMessage = "You Are Awesome!"
+//        let greatMessage = "You Are Great!"
+//
+//        if messageLabel.text == awesomeMessage {
+//            messageLabel.text = greatMessage
+//            imageView.image = UIImage(named: "image2")
+//        } else if messageLabel.text == greatMessage {
+//            messageLabel.text = "You Are Da Bomb!"
+//            imageView.image = UIImage(named: "image1")
+//
+//        } else {
+//            messageLabel.text = awesomeMessage
+//            imageView.image = UIImage(named: "image0")
+//        }
         imageView.isHidden = false
         
     }
