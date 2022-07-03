c8738b0cef98ab3e392c5c4d6675f4241b53f104
Scott Rocca <scott.rocca@hp.com>
2022-07-03 15:55:21 -0700

    Refactoring

--- You Are Awesome!/ViewController.swift
+++ You Are Awesome!/ViewController.swift
@@ -14,7 +14,9 @@ class ViewController: UIViewController {
     
     var currentImageIndex = -1
     var currentMessageIndex = -1
-    let totalNumberOfImages = 9
+    var currentSoundIndex = -1
+    let totalNumberOfImages = 10
+    let totalNumberOfSounds = 6
     var audioPlayer: AVAudioPlayer!
     
     override func viewDidLoad() {
@@ -22,30 +24,8 @@ class ViewController: UIViewController {
         messageLabel.text = "Click \"Show Message\""
     }
     
-    @IBAction func messageButtonPressed(_ sender: UIButton) {
-        print("Button was pressed")
-        let messages = ["You Are Awesome!",
-                        "You Are Great!",
-                        "You Are Da Bomb!",
-                        "You are Fantastic",
-                        "Fabulous? That's You!",
-                        "You've Got the Design Skills of Jony Ive"]
-        
-        var newMessageIndex: Int
-        repeat {
-            newMessageIndex = Int.random(in: 0..<messages.count)
-        } while newMessageIndex == currentMessageIndex
-        currentMessageIndex = newMessageIndex
-        messageLabel.text = messages[currentMessageIndex]
-        
-        var newImageIndex: Int
-        repeat {
-            newImageIndex = Int.random(in: 0...totalNumberOfImages)
-        } while newImageIndex == currentImageIndex
-        currentImageIndex = newImageIndex
-        imageView.image = UIImage(named: "image\(currentImageIndex)")
-        
-        if let sound = NSDataAsset(name: "sound0"){
+    func playSound(name: String) {
+        if let sound = NSDataAsset(name: name){
             do {
                 try audioPlayer = AVAudioPlayer(data: sound.data)
                 audioPlayer.play()
@@ -56,45 +36,75 @@ class ViewController: UIViewController {
         } else {
             print("😡 ERROR: Could not read data from file sound0")
         }
+    }
+    
+    func nonRepeatingRandom(originalNumber: Int, upperBounds: Int) -> Int {
+        var newIndex: Int
+        repeat {
+            newIndex = Int.random(in: 0...upperBounds)
+        } while newIndex == originalNumber
         
-//        var newMessage = messages[Int.random(in: 0...messages.count-1)]
-//        while newMessage == messageLabel.text {
-//            newMessage = messages[Int.random(in: 0...messages.count-1)]
-//        }
-//        messageLabel.text = newMessage
-//
-//
-//        var newImage = UIImage(named: "image\(Int.random(in: 0...totalNumberOfImages))")
-//        while newImage == imageView.image {
-//            newImage = UIImage(named: "image\(Int.random(in: 0...totalNumberOfImages))")
-//        }
-//        imageView.image = newImage
+        return newIndex
+    }
+    
+    @IBAction func messageButtonPressed(_ sender: UIButton) {
+        let messages = ["You Are Awesome!",
+                        "You Are Great!",
+                        "You Are Da Bomb!",
+                        "You are Fantastic",
+                        "Fabulous? That's You!",
+                        "You've Got the Design Skills of Jony Ive"]
+        
+
+        currentMessageIndex = nonRepeatingRandom(originalNumber: currentMessageIndex, upperBounds: messages.count-1)
+        messageLabel.text = messages[currentMessageIndex]
+        
+        currentImageIndex = nonRepeatingRandom(originalNumber: currentImageIndex, upperBounds: totalNumberOfImages-1)
+        imageView.image = UIImage(named: "image\(currentImageIndex)")
+        
+        currentSoundIndex = nonRepeatingRandom(originalNumber: currentSoundIndex, upperBounds: totalNumberOfSounds-1 )
+        playSound(name: "sound\(currentSoundIndex)")
         
         
-//        messageLabel.text = messages[currentMessageIndex]
-//        imageView.image = UIImage(named: "image\(currentMessageIndex)")
-//
-//        currentMessageIndex += 1
-//        if (currentMessageIndex == messages.count ) {
-//            currentMessageIndex = 0
-//        }
-//
+        
+        //        var newMessage = messages[Int.random(in: 0...messages.count-1)]
+        //        while newMessage == messageLabel.text {
+        //            newMessage = messages[Int.random(in: 0...messages.count-1)]
+        //        }
+        //        messageLabel.text = newMessage
+        //
+        //
+        //        var newImage = UIImage(named: "image\(Int.random(in: 0...totalNumberOfImages))")
+        //        while newImage == imageView.image {
+        //            newImage = UIImage(named: "image\(Int.random(in: 0...totalNumberOfImages))")
+        //        }
+        //        imageView.image = newImage
         
         
-//        let awesomeMessage = "You Are Awesome!"
-//        let greatMessage = "You Are Great!"
-//
-//        if messageLabel.text == awesomeMessage {
-//            messageLabel.text = greatMessage
-//            imageView.image = UIImage(named: "image2")
-//        } else if messageLabel.text == greatMessage {
-//            messageLabel.text = "You Are Da Bomb!"
-//            imageView.image = UIImage(named: "image1")
-//
-//        } else {
-//            messageLabel.text = awesomeMessage
-//            imageView.image = UIImage(named: "image0")
-//        }
+        //        messageLabel.text = messages[currentMessageIndex]
+        //        imageView.image = UIImage(named: "image\(currentMessageIndex)")
+        //
+        //        currentMessageIndex += 1
+        //        if (currentMessageIndex == messages.count ) {
+        //            currentMessageIndex = 0
+        //        }
+        //
+        
+        
+        //        let awesomeMessage = "You Are Awesome!"
+        //        let greatMessage = "You Are Great!"
+        //
+        //        if messageLabel.text == awesomeMessage {
+        //            messageLabel.text = greatMessage
+        //            imageView.image = UIImage(named: "image2")
+        //        } else if messageLabel.text == greatMessage {
+        //            messageLabel.text = "You Are Da Bomb!"
+        //            imageView.image = UIImage(named: "image1")
+        //
+        //        } else {
+        //            messageLabel.text = awesomeMessage
+        //            imageView.image = UIImage(named: "image0")
+        //        }
         imageView.isHidden = false
         
     }
